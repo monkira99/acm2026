de149cf1957937ec8b7a5ceef3fd934d6149f9a1
tiennd.vuihoc <tiennd@vuihoc.vn>
2026-06-07 21:55:47 +0700

    fix(committees): correct affiliation titles for consistency and accuracy

--- src/data/committees.ts
+++ src/data/committees.ts
@@ -21,7 +21,7 @@ export const committees: Committee[] = [
         name: "Ms. Funabiki Rie",
         titleLines: [""],
         affiliation:
-          "Senior Chief, CBD Administration Division of Biological Resource Center (NBRC), National Institute of Technology and Evaluation (NITE)",
+          "Senior Chief, CBD Administration Division of the Biological Resource Center (NBRC), National Institute of Technology and Evaluation (NITE)",
         image: "/images/committees/MS_FUNABIKI_RIE.jpeg",
       },
       {
@@ -30,7 +30,7 @@ export const committees: Committee[] = [
           ""
         ],
         affiliation:
-          "Director of Agricultural Culture Collection of China, Institute of Agricultural Resources and Regional Planning, Chinese Academy of Agricultural Sciences\nDirector of National Microbial Resource Center",
+          "Director of the Agricultural Culture Collection of China, Institute of Agricultural Resources and Regional Planning, Chinese Academy of Agricultural Sciences\nDirector of the National Microbial Resource Center",
         image: "/images/committees/DR_HAILEI_WEI.png",
       },
       {
@@ -39,13 +39,13 @@ export const committees: Committee[] = [
           "",
         ],
         affiliation:
-          "Director of The Institute of Bioscience, Universiti Putra Malaysia",
+          "Director of the Institute of Bioscience, Universiti Putra Malaysia",
         image: "/images/committees/DR_HSIU_JUNG_LO.png",
       },
       {
         name: "Dr. Ju Huck Lee",
         titleLines: [""],
-        affiliation: "Director of Korean Collection for Type Cultures (KCTC)",
+        affiliation: "Director of the Korean Collection for Type Cultures (KCTC)",
         image: "/images/committees/DR_JU_HUCK_LEE.png",
       },
       {
@@ -54,7 +54,7 @@ export const committees: Committee[] = [
           "",
         ],
         affiliation:
-          "Director for Biodiversity Policy Planning and Coordination / Bio-manufacturing Planning and Coordination of Biological Resource Center (NBRC), National Institute of Technology and Evaluation (NITE)",
+          "Director for Biodiversity Policy Planning and Coordination / Bio-manufacturing Planning and Coordination of the Biological Resource Center (NBRC), National Institute of Technology and Evaluation (NITE)",
         image: "/images/committees/DR_KINOSHITA_HIROSHI.png",
       },
       {
@@ -63,7 +63,7 @@ export const committees: Committee[] = [
           "",
         ],
         affiliation:
-          "Director of The Institute of Bioscience, Universiti Putra Malaysia",
+          "Director of the Institute of Bioscience, Universiti Putra Malaysia",
         image: "/images/committees/PROFESSOR_DR_ ZUNITA_ZAKARIA.png",
       },
       {
@@ -72,7 +72,7 @@ export const committees: Committee[] = [
           "",
         ],
         affiliation:
-          "Director of The Institute of Microbiology and Biotechnology (IMBT), Vietnam National University",
+          "Director of the Institute of Microbiology and Biotechnology (IMBT), Vietnam National University",
         image: "/images/welcome/Dr.Trinh_Thanh_Trung.webp",
       },
       {
@@ -81,7 +81,7 @@ export const committees: Committee[] = [
           "",
         ],
         affiliation:
-          "Head of Vietnam Type Culture Collection, Institute of Microbiology and Biotechnology (IMBT), Vietnam National University",
+          "Head of the Vietnam Type Culture Collection, Institute of Microbiology and Biotechnology (IMBT), Vietnam National University",
         image: "/images/committees/DR_HOANG_THI_LAN_ANH.png",
       },
       {
@@ -90,7 +90,7 @@ export const committees: Committee[] = [
           "",
         ],
         affiliation:
-          "Director of Korean Agricultural Culture Collection (KACC), Rural Development Administration (RDA)",
+          "Director of the Korean Agricultural Culture Collection (KACC), Rural Development Administration (RDA)",
         image: "/images/committees/DR_SEUNG_BEOM_HONG.jpeg",
       },
       {
